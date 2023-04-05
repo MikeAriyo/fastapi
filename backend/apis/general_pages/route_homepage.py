0d6f99057a1e65dc8b41ac32b5194fa8e9d55227
MikeAriyo <ariyoadekola2@gmail.com>
2023-04-05 10:24:41 +0100

    added the homepage template

--- backend/apis/general_pages/route_homepage.py
+++ backend/apis/general_pages/route_homepage.py
@@ -1,11 +1,15 @@
+# modules imports
 from fastapi import APIRouter
 from fastapi import Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
-templates = Jinja2Templates(directory="templates")
-general_pages_router = APIRouter()
+templates = Jinja2Templates(directory="templates")  # search for html files inside the templates folder
 
+
+general_pages_router = APIRouter() # instance of the api router
+
+# capture the actual request and return an HTMLResponse 
 @general_pages_router.get("/")
 async def home(request: Request):
 	return templates.TemplateResponse("general_pages/homepage.html",{"request":request})
